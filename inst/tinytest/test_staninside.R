c81086e78021e65a482bc8b34a86064602809dca
Michael DeWitt <me.dewitt.jr@gmail.com>
2021-08-09 05:43:03 -0400

    fixed tests

--- inst/tinytest/test_staninside.R
+++ inst/tinytest/test_staninside.R
@@ -1,7 +1,7 @@
 
 # Placeholder with simple test
 expect_equal(info = "Test function can find Stan",
-						 staninside::find_stan_code(pkgname = "staninside")$base_stan_location[[1]],
+						 staninside::find_stan_code(pkgname = "staninside")$base_stan_location[[2]],
 						 "test.stan")
 
 expect_equal(info = "Test S3 for Print",class(staninside::find_stan_code(pkgname = "staninside")$stan_pkg_contents$test.stan),
